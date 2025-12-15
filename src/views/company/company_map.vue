0e72e48433ef94dcb5f30370bc8c91ead186e700
ohyejin <ohyejin355@gmail.com>
2025-12-15 10:51:23 +0900

    공통 컴포넌트 부분 삭제

--- src/views/company/company_map.vue
+++ src/views/company/company_map.vue
@@ -1,17 +1,5 @@
 <template>
   <main>
-    <div class="relative pt-16 md:pt-20">
-      <div
-        class="relative py-24 md:py-32 bg-cover bg-center"
-        style="background-image: url(/image/sub/sub_visual_img_bg.png)"
-      >
-        <div class="absolute inset-0 bg-slate-900 opacity-60"></div>
-        <div class="relative max-w-7xl mx-auto px-6 lg:px-8 text-center">
-          <h1 class="text-3xl md:text-4xl font-black text-white">회사소개</h1>
-        </div>
-      </div>
-    </div>
-
     <section class="py-16 md:py-24">
       <div class="max-w-7xl mx-auto px-6 lg:px-8">
         <div class="bg-white p-6 md:p-12 rounded-lg shadow-lg">
