8b2e9b3be30ad30189ff784d1c9af7cbffc0c506
ohyejin <ohyejin355@gmail.com>
2025-12-15 17:53:01 +0900

    카카오맵 api 적용

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import '@/css/common.css'
 import '@/css/tailwind.css'
+import { useKakao } from 'vue3-kakao-maps/@utils';
+
+useKakao('appkey');
 
 const app = createApp(App)
 
