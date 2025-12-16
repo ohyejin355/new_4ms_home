09b8d3e03cab0a1baf9b4564e61a13e9dcb9de0c
ohyejin <ohyejin355@gmail.com>
2025-12-16 13:54:20 +0900

    tailwind 수정

--- src/views/project/PJT_01.vue
+++ src/views/project/PJT_01.vue
@@ -3,32 +3,50 @@
     <section class="section">
       <div class="wrapper">
         <div class="text-center mb-16">
-          <h2 class="text-[1.875rem]/[36px] font-black text-slate-900 mb-4 md:text-[2.25rem]/[36px] font-mont">주요프로젝트</h2>
-          <p class="text-[1.125rem]/[18px] text-slate-600">주식회사 포엠에스는 다양한 업무를 수행하고 있습니다.</p>
+          <h2
+            class="text-[1.875rem]/[36px] font-black text-slate-900 mb-4 md:text-[2.25rem]/[36px] font-mont"
+          >
+            주요프로젝트
+          </h2>
+          <p class="text-[1.125rem]/[18px] text-slate-600">
+            주식회사 포엠에스는 다양한 업무를 수행하고 있습니다.
+          </p>
         </div>
 
         <div class="flex gap-2 flex-wrap mb-12 justify-center">
           <a
-            class="text-slate-700 py-3 px-6 bg-slate-200 rounded-md cursor-pointer text-xs leading-[18px] hover:text-white hover:bg-teal-700 hover:shadow-lg aria-selected:text-white aria-selected:bg-teal-700 aria-selected:shadow-lg"
+            class="text-slate-700 py-3 px-6 bg-slate-200 rounded-md cursor-pointer text-xs leading-4.5 hover:text-white hover:bg-teal-700 hover:shadow-lg aria-selected:text-white aria-selected:bg-teal-700 aria-selected:shadow-lg"
             v-for="year in uniqueYears"
             :key="year"
             :aria-selected="selectedYear === year"
-            @click="onSelectYear(year)">{{ year }}년</a>
+            @click="onSelectYear(year)"
+            >{{ year }}년</a
+          >
         </div>
 
         <div class="flex">
           <div class="layout-pc font-bold text-[1.5rem] leading-8 w-1/6 text-slate-800">
             {{ selectedYear }}
           </div>
-          <div class="flex flex-col gap-6 w-full md:pl-8 md:border-l-2 md:border-slate-200 md:w-5/6">
+          <div
+            class="flex flex-col gap-6 w-full md:pl-8 md:border-l-2 md:border-slate-200 md:w-5/6"
+          >
             <div
               v-for="project in projectsByYear"
               :key="project.year + project.month + project.partner + project.name"
               class="border-b border-slate-200 gap-4 items-center pb-4 md:grid md:grid-cols-12"
             >
-              <p class="text-slate-500 font-medium text-sm leading-5 md:col-span-2">{{ project.year }}.{{ project.month }}</p>
-              <p class="text-slate-800 leading-7 font-semibold md:text-base md:leading-6 md:col-span-4">{{ project.partner }}</p>
-              <h3 class="leading-[18px] text-[14px] text-slate-600 md:col-span-6">{{ project.name }}</h3>
+              <p class="text-slate-500 font-medium text-sm leading-5 md:col-span-2">
+                {{ project.year }}.{{ project.month }}
+              </p>
+              <p
+                class="text-slate-800 leading-7 font-semibold md:text-base md:leading-6 md:col-span-4"
+              >
+                {{ project.partner }}
+              </p>
+              <h3 class="leading-4.5 text-[14px] text-slate-600 md:col-span-6">
+                {{ project.name }}
+              </h3>
             </div>
           </div>
         </div>
@@ -38,18 +56,16 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue';
-  import Projects from './temp_projects.js';
+import { ref } from 'vue'
+import Projects from './temp_projects.js'
 
-  const years = Projects.value.map(project => project.year);
-  const uniqueYears = [...new Set(years)].sort((a, b) => b - a);
-  const selectedYear = ref(uniqueYears[0]);
-  const projectsByYear = ref(Projects.value.filter(project => project.year === selectedYear.value));
+const years = Projects.value.map((project) => project.year)
+const uniqueYears = [...new Set(years)].sort((a, b) => b - a)
+const selectedYear = ref(uniqueYears[0])
+const projectsByYear = ref(Projects.value.filter((project) => project.year === selectedYear.value))
 
-  const onSelectYear = (year) => {
-    selectedYear.value = year;
-    projectsByYear.value = Projects.value.filter(project => project.year === year);
-  }
+const onSelectYear = (year) => {
+  selectedYear.value = year
+  projectsByYear.value = Projects.value.filter((project) => project.year === year)
+}
 </script>
-
-
--- src/views/project/PJT_02.vue
+++ src/views/project/PJT_02.vue
@@ -3,12 +3,24 @@
     <section class="section">
       <div class="wrapper">
         <div class="text-center mb-16">
-          <h2 class="text-[1.875rem]/[36px] font-black text-slate-900 mb-4 md:text-[2.25rem]/[36px] font-mont">주요협력사</h2>
-          <p class="text-[1.125rem]/[18px] text-slate-600">다양한 기업들과 협력하여 시스템 개발에 힘쓰고 있습니다.</p>
+          <h2
+            class="text-[1.875rem]/[36px] font-black text-slate-900 mb-4 md:text-[2.25rem]/[36px] font-mont"
+          >
+            주요협력사
+          </h2>
+          <p class="text-[1.125rem]/[18px] text-slate-600">
+            다양한 기업들과 협력하여 시스템 개발에 힘쓰고 있습니다.
+          </p>
         </div>
 
-        <div class="parter-grid grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-8 max-w-[1050px] mx-auto">
-          <div class="partner-item flex shadow-md p-8 bg-white rounded-lg justify-center items-center h-32 border border-gray-200" v-for="(partner, index) in partnerLogo" :key="index">
+        <div
+          class="parter-grid grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-8 max-w-262.5 mx-auto"
+        >
+          <div
+            class="partner-item flex shadow-md p-8 bg-white rounded-lg justify-center items-center h-32 border border-gray-200"
+            v-for="(partner, index) in partnerLogo"
+            :key="index"
+          >
             <img :src="partner.logoUrl" :alt="partner.name" class="max-h-16" />
           </div>
         </div>
@@ -18,20 +30,20 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue';
+import { ref } from 'vue'
 
-  const partnerLogo = ref([
-    { logoUrl: '/image/sub/pro_logo_01.png', name: 'Partner 1' },
-    { logoUrl: '/image/sub/pro_logo_02.png', name: 'Partner 2' },
-    { logoUrl: '/image/sub/pro_logo_03.png', name: 'Partner 3' },
-    { logoUrl: '/image/sub/pro_logo_04.png', name: 'Partner 4' },
-    { logoUrl: '/image/sub/pro_logo_05.png', name: 'Partner 5' },
-    { logoUrl: '/image/sub/pro_logo_06.png', name: 'Partner 6' },
-    { logoUrl: '/image/sub/pro_logo_07.png', name: 'Partner 7' },
-    { logoUrl: '/image/sub/pro_logo_08.png', name: 'Partner 8' },
-    { logoUrl: '/image/sub/pro_logo_09.png', name: 'Partner 9' },
-    { logoUrl: '/image/sub/pro_logo_10.png', name: 'Partner 10' },
-    { logoUrl: '/image/sub/pro_logo_11.png', name: 'Partner 11' },
-    { logoUrl: '/image/sub/pro_logo_12.png', name: 'Partner 12' },
-  ])
+const partnerLogo = ref([
+  { logoUrl: '/image/sub/pro_logo_01.png', name: 'Partner 1' },
+  { logoUrl: '/image/sub/pro_logo_02.png', name: 'Partner 2' },
+  { logoUrl: '/image/sub/pro_logo_03.png', name: 'Partner 3' },
+  { logoUrl: '/image/sub/pro_logo_04.png', name: 'Partner 4' },
+  { logoUrl: '/image/sub/pro_logo_05.png', name: 'Partner 5' },
+  { logoUrl: '/image/sub/pro_logo_06.png', name: 'Partner 6' },
+  { logoUrl: '/image/sub/pro_logo_07.png', name: 'Partner 7' },
+  { logoUrl: '/image/sub/pro_logo_08.png', name: 'Partner 8' },
+  { logoUrl: '/image/sub/pro_logo_09.png', name: 'Partner 9' },
+  { logoUrl: '/image/sub/pro_logo_10.png', name: 'Partner 10' },
+  { logoUrl: '/image/sub/pro_logo_11.png', name: 'Partner 11' },
+  { logoUrl: '/image/sub/pro_logo_12.png', name: 'Partner 12' },
+])
 </script>
