4bdbca69aaf40c905f396de52d5aa6ee959faa75
ohyejin <ohyejin355@gmail.com>
2025-12-17 10:04:50 +0900

    hover css 효과 주석 처리

--- src/views/company/company_info.vue
+++ src/views/company/company_info.vue
@@ -168,11 +168,11 @@
     0 4px 6px -2px rgba(0, 0, 0, 0.05);
 }
 
-.white_box:hover {
+/* .white_box:hover {
   box-shadow:
     0 20px 25px -5px rgba(0, 0, 0, 0.1),
     0 10px 10px -5px rgba(0, 0, 0, 0.04);
-}
+} */
 
 .box_div {
   display: flex;
